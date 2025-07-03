793242afdf03e338795a5d16947c62e2c40f69b2
adityanurdin <adityanurdin0@gmail.com>
2025-07-03 15:21:15 +0700

    update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
+use \App\Models\Student;
+use \App\Models\InternshipRequest;
+
 class DashboardController extends Controller
 {
     public function __construct()
@@ -14,6 +17,15 @@ class DashboardController extends Controller
 
     public function index()
     {
-        return view('dashboard');
+
+        $students = Student::get()->count();
+        $internship_request  = InternshipRequest::query()->with('user', 'company');
+        $data = array(
+            'student_count'         => $students,
+            'internship_requests'   => $internship_request->orderBy('created_at', 'desc')->limit(5)->get(),
+            'pending_request'       => $internship_request->where('status', 'pending')->get()->count()
+        );
+
+        return view('dashboard', compact('data'));
     }
 }
